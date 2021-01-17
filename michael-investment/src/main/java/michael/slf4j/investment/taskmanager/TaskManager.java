40572e313910dccada08f1fa9f69ceffe28f7153
Michael <363966403@qq.com>
2021-01-17 23:18:04 +0800

    add task manager

---
+++ michael-investment/src/main/java/michael/slf4j/investment/taskmanager/TaskManager.java
@@ -0,0 +1,51 @@
+package michael.slf4j.investment.taskmanager;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import michael.slf4j.investment.etl.FutureLoader;
+import michael.slf4j.investment.util.FutureContract;
+
+@Controller
+public class TaskManager {
+	@Autowired
+	private FutureLoader futureLoader;
+	
+	private ScheduledExecutorService service = Executors.newScheduledThreadPool(20);
+	private Map<String, Boolean> recordMap = new ConcurrentHashMap<>();
+	private CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+	
+	public boolean scheduleTask(String variety) {
+		if(recordMap.get(variety) == null || !recordMap.get(variety)) {
+			recordMap.put(variety, true);
+		} else {
+			return false;
+		}
+		List<String> list = FutureContract.getFutureContracts(variety);
+		for (String security : list) {
+			FutureTask task = new FutureTask(futureLoader, httpClient, security);
+			service.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
+		}
+		return true;
+	}
+	
+	public void close() {
+		try {
+			httpClient.close();
+			service.shutdown();
+		} catch (IOException e) {
+		}
+	}
+	
+
+}
