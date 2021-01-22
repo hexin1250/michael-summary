a97f74e7efc00736125ae52b7b7746d946b21ed7
hexin1250 <363966403@qq.com>
2021-01-22 10:24:56 +0800

    remove testcase.

--- michael-service/src/test/java/michael/slf4j/demo/controller/GreetingControllerTest.java
+++ michael-service/src/test/java/michael/slf4j/demo/controller/GreetingControllerTest.java
@@ -1,9 +1,6 @@
 package michael.slf4j.demo.controller;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Test;
 
 public class GreetingControllerTest {
 	GreetingController controller = null;
@@ -11,11 +8,6 @@ public class GreetingControllerTest {
 	@Before
 	public void setup() {
 		controller = new GreetingController();
-	}
-
-	@Test
-	public void testIndex() {
-		assertEquals("Hello World!", controller.index());
 	}
 
 }
