6a9ab01053f1e4460bb89051e9b854225f57b8a0
Michael <363966403@qq.com>
2021-05-19 14:24:37 +0800

    add J

--- michael-investment-data-loader/src/main/java/michael/slf4j/investment/util/LoadFutureData.java
+++ michael-investment-data-loader/src/main/java/michael/slf4j/investment/util/LoadFutureData.java
@@ -38,151 +38,154 @@ public class LoadFutureData {
 		String driverClass = "com.mysql.cj.jdbc.Driver";
 		Class.forName(driverClass);
 
-		String dirName = "src/main/data/I";
+		String dirName = "src/main/data";
 		String url = "jdbc:mysql://localhost:3306/investment?useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Shanghai";
 		String user = "springuser";
 		String password = "springuser";
 		int count = 0;
 		try (Connection conn = DriverManager.getConnection(url, user, password);
 				PreparedStatement ps = conn.prepareStatement(SQL_TEMPLATE)) {
-			Set<String> tradeDates = getTradeDates(conn);
 			File dir = new File(dirName);
-			Map<String, Map<String, TimeseriesModel>> tradeMap = new HashMap<>();
-			for (File file : dir.listFiles()) {
-				System.out.println("Start to read file[" + file.getName() + "].");
-				try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-					String line = null;
-					while ((line = br.readLine()) != null) {
-						String[] parts = line.split("#####");
-						Matcher m = pattern.matcher(parts[0]);
-						if (m.matches()) {
-							String tradeTs = m.group(1);
-							Date date = sdf.parse(tradeTs);
-							LocalDateTime ld = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-							String tradeDate = dateSdf.format(date);
-							int hour = ld.getHour();
-							if(hour > 20) {
-								int dw = ld.getDayOfWeek().getValue();
-								if(dw == 5) {
-									ld = ld.plusDays(3);
-								} else {
-									ld = ld.plusDays(1);
+			for (File varietyDir : dir.listFiles()) {
+				Set<String> tradeDates = getTradeDates(conn, varietyDir.getName());
+				Map<String, Map<String, TimeseriesModel>> tradeMap = new HashMap<>();
+				for (File file : varietyDir.listFiles()) {
+					System.out.println("Start to read file[" + file.getName() + "].");
+					try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+						String line = null;
+						while ((line = br.readLine()) != null) {
+							String[] parts = line.split("#####");
+							Matcher m = pattern.matcher(parts[0]);
+							if (m.matches()) {
+								String tradeTs = m.group(1);
+								Date date = sdf.parse(tradeTs);
+								LocalDateTime ld = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+								String tradeDate = dateSdf.format(date);
+								int hour = ld.getHour();
+								if(hour > 20) {
+									int dw = ld.getDayOfWeek().getValue();
+									if(dw == 5) {
+										ld = ld.plusDays(3);
+									} else {
+										ld = ld.plusDays(1);
+									}
+									tradeDate = ld.format(DateTimeFormatter.ISO_DATE);
 								}
-								tradeDate = ld.format(DateTimeFormatter.ISO_DATE);
-							}
-							Map<String, TimeseriesModel> tdMap = tradeMap.get(tradeDate);
-							if(tdMap == null) {
-								if(!tradeDates.contains(tradeDate)) {
-									for (Map<String, TimeseriesModel> map : tradeMap.values()) {
-										for (TimeseriesModel tm : map.values()) {
-											ps.setString(1, tm.getSecurity());
-											ps.setString(2, tm.getName());
-											ps.setBigDecimal(3, tm.getOpen());
-											ps.setBigDecimal(4, tm.getHigh());
-											ps.setBigDecimal(5, tm.getLow());
-											ps.setBigDecimal(6, tm.getClose());
-											ps.setBigDecimal(7, tm.getUpLimit());
-											ps.setBigDecimal(8, tm.getDownLimit());
-											ps.setBigDecimal(9, tm.getVolume());
-											ps.setString(10, "1D");
-											ps.setString(11, tm.getTradeDate());
-											ps.setString(12, tm.getTradeTs());
-											ps.setString(13, tm.getIsMainFuture());
-											ps.setString(14, tm.getSecurity().replaceAll("[\\d]+", ""));
-											ps.setBigDecimal(15, tm.getOpenInterest());
-											ps.addBatch();
+								Map<String, TimeseriesModel> tdMap = tradeMap.get(tradeDate);
+								if(tdMap == null) {
+									if(!tradeDates.contains(tradeDate)) {
+										for (Map<String, TimeseriesModel> map : tradeMap.values()) {
+											for (TimeseriesModel tm : map.values()) {
+												ps.setString(1, tm.getSecurity());
+												ps.setString(2, tm.getName());
+												ps.setBigDecimal(3, tm.getOpen());
+												ps.setBigDecimal(4, tm.getHigh());
+												ps.setBigDecimal(5, tm.getLow());
+												ps.setBigDecimal(6, tm.getClose());
+												ps.setBigDecimal(7, tm.getUpLimit());
+												ps.setBigDecimal(8, tm.getDownLimit());
+												ps.setBigDecimal(9, tm.getVolume());
+												ps.setString(10, "1D");
+												ps.setString(11, tm.getTradeDate());
+												ps.setString(12, tm.getTradeTs());
+												ps.setString(13, tm.getIsMainFuture());
+												ps.setString(14, tm.getSecurity().replaceAll("[\\d]+", ""));
+												ps.setBigDecimal(15, tm.getOpenInterest());
+												ps.addBatch();
+											}
 										}
 									}
+									tradeMap.clear();
+									
+									tdMap = new LinkedHashMap<>();
+									tradeMap.put(tradeDate, tdMap);
 								}
-								tradeMap.clear();
-								
-								tdMap = new LinkedHashMap<>();
-								tradeMap.put(tradeDate, tdMap);
-							}
-							String mainFuture = parts[1];
-							for (int i = 2; i < parts.length; i++) {
-								String[] futureParts = parts[i].split(",");
-								if("nan".equalsIgnoreCase(futureParts[1])) {
-									continue;
-								}
-								TimeseriesModel tm = new TimeseriesModel();
-								String security = PREFIX + futureParts[0].substring(futureParts[0].length() - 4, futureParts[0].length());
-								tm.setSecurity(security);
-								tm.setName(futureParts[0]);
-								tm.setOpen(new BigDecimal(futureParts[1]));
-								tm.setHigh(new BigDecimal(futureParts[2]));
-								tm.setLow(new BigDecimal(futureParts[3]));
-								tm.setClose(new BigDecimal(futureParts[4]));
-								tm.setUpLimit(new BigDecimal(futureParts[5]));
-								tm.setDownLimit(new BigDecimal(futureParts[6]));
-								tm.setVolume(new BigDecimal(futureParts[7]));
-								tm.setOpenInterest(new BigDecimal(futureParts[8]));
-								tm.setTradeDate(tradeDate);
-								tm.setTradeTs(tradeTs);
-								tm.setFreq("1MI");
-								String isMainFuture = null;
-								if (security.equals(mainFuture)) {
-									isMainFuture = "T";
-								} else {
-									isMainFuture = "F";
-								}
-								tm.setIsMainFuture(isMainFuture);
-								
-								if(!tradeDates.contains(tradeDate)) {
-									ps.setString(1, tm.getSecurity());
-									ps.setString(2, tm.getName());
-									ps.setBigDecimal(3, tm.getOpen());
-									ps.setBigDecimal(4, tm.getHigh());
-									ps.setBigDecimal(5, tm.getLow());
-									ps.setBigDecimal(6, tm.getClose());
-									ps.setBigDecimal(7, tm.getUpLimit());
-									ps.setBigDecimal(8, tm.getDownLimit());
-									ps.setBigDecimal(9, tm.getVolume());
-									ps.setString(10, tm.getFreq());
-									ps.setString(11, tm.getTradeDate());
-									ps.setString(12, tm.getTradeTs());
-									ps.setString(13, tm.getIsMainFuture());
-									ps.setString(14, tm.getSecurity().replaceAll("[\\d]+", ""));
-									ps.setBigDecimal(15, tm.getOpenInterest());
-									ps.addBatch();
-									count++;
-									if (count >= 300) {
-										ps.executeBatch();
-										count = 0;
+								String mainFuture = parts[1];
+								for (int i = 2; i < parts.length; i++) {
+									String[] futureParts = parts[i].split(",");
+									if("nan".equalsIgnoreCase(futureParts[1])) {
+										continue;
 									}
-								}
-								
-								TimeseriesModel dm = tdMap.get(security);
-								if(dm == null) {
-									dm = TimeseriesModel.of(tm);
-									tdMap.put(security, dm);
-								} else {
-									dm.setClose(tm.getClose());
-									dm.setTradeTs(tm.getTradeTs());
-									dm.setVolume(dm.getVolume().add(tm.getVolume()));
-									if(dm.getHigh().compareTo(tm.getHigh()) < 0) {
-										dm.setHigh(tm.getHigh());
+									TimeseriesModel tm = new TimeseriesModel();
+									String security = PREFIX + futureParts[0].substring(futureParts[0].length() - 4, futureParts[0].length());
+									tm.setSecurity(security);
+									tm.setName(futureParts[0]);
+									tm.setOpen(new BigDecimal(futureParts[1]));
+									tm.setHigh(new BigDecimal(futureParts[2]));
+									tm.setLow(new BigDecimal(futureParts[3]));
+									tm.setClose(new BigDecimal(futureParts[4]));
+									tm.setUpLimit(new BigDecimal(futureParts[5]));
+									tm.setDownLimit(new BigDecimal(futureParts[6]));
+									tm.setVolume(new BigDecimal(futureParts[7]));
+									tm.setOpenInterest(new BigDecimal(futureParts[8]));
+									tm.setTradeDate(tradeDate);
+									tm.setTradeTs(tradeTs);
+									tm.setFreq("1MI");
+									String isMainFuture = null;
+									if (security.equals(mainFuture)) {
+										isMainFuture = "T";
+									} else {
+										isMainFuture = "F";
 									}
-									if(dm.getLow().compareTo(tm.getLow()) > 0) {
-										dm.setLow(tm.getLow());
+									tm.setIsMainFuture(isMainFuture);
+									
+									if(!tradeDates.contains(tradeDate)) {
+										ps.setString(1, tm.getSecurity());
+										ps.setString(2, tm.getName());
+										ps.setBigDecimal(3, tm.getOpen());
+										ps.setBigDecimal(4, tm.getHigh());
+										ps.setBigDecimal(5, tm.getLow());
+										ps.setBigDecimal(6, tm.getClose());
+										ps.setBigDecimal(7, tm.getUpLimit());
+										ps.setBigDecimal(8, tm.getDownLimit());
+										ps.setBigDecimal(9, tm.getVolume());
+										ps.setString(10, tm.getFreq());
+										ps.setString(11, tm.getTradeDate());
+										ps.setString(12, tm.getTradeTs());
+										ps.setString(13, tm.getIsMainFuture());
+										ps.setString(14, tm.getSecurity().replaceAll("[\\d]+", ""));
+										ps.setBigDecimal(15, tm.getOpenInterest());
+										ps.addBatch();
+										count++;
+										if (count >= 300) {
+											ps.executeBatch();
+											count = 0;
+										}
+									}
+									
+									TimeseriesModel dm = tdMap.get(security);
+									if(dm == null) {
+										dm = TimeseriesModel.of(tm);
+										tdMap.put(security, dm);
+									} else {
+										dm.setClose(tm.getClose());
+										dm.setTradeTs(tm.getTradeTs());
+										dm.setVolume(dm.getVolume().add(tm.getVolume()));
+										if(dm.getHigh().compareTo(tm.getHigh()) < 0) {
+											dm.setHigh(tm.getHigh());
+										}
+										if(dm.getLow().compareTo(tm.getLow()) > 0) {
+											dm.setLow(tm.getLow());
+										}
 									}
 								}
 							}
 						}
 					}
 				}
-			}
-			if (count > 0) {
-				ps.executeBatch();
-				count = 0;
+				if (count > 0) {
+					ps.executeBatch();
+					count = 0;
+				}
 			}
 		}
 		System.out.println("Done.");
 	}
 	
-	private static Set<String> getTradeDates(Connection conn) throws SQLException{
-		try(PreparedStatement ps = conn.prepareStatement("select distinct trade_date from timeseries")) {
+	private static Set<String> getTradeDates(Connection conn, String variety) throws SQLException{
+		try(PreparedStatement ps = conn.prepareStatement("select distinct trade_date from timeseries where variety = ?")) {
 			Set<String> ret = new HashSet<>();
+			ps.setString(1, variety);
 			ResultSet rs = ps.executeQuery();
 			while(rs.next()) {
 				ret.add(rs.getString(1));
