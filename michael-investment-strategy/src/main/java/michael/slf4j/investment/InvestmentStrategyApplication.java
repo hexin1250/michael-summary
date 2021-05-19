14607f908732dcd29fddd869fab10fe7259a4665
Michael <363966403@qq.com>
2021-05-19 13:43:15 +0800

    update

---
+++ michael-investment-strategy/src/main/java/michael/slf4j/investment/InvestmentStrategyApplication.java
@@ -0,0 +1,11 @@
+package michael.slf4j.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentStrategyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentStrategyApplication.class, args);
+	}
+}
