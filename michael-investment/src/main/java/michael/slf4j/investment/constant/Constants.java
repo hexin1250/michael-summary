7852504050e6c6107075dc80022d2fb439bcc5f2
Michael <363966403@qq.com>
2021-05-20 17:08:57 +0800

    add RB

--- michael-investment/src/main/java/michael/slf4j/investment/constant/Constants.java
+++ michael-investment/src/main/java/michael/slf4j/investment/constant/Constants.java
@@ -3,6 +3,6 @@ package michael.slf4j.investment.constant;
 import java.util.List;
 
 public class Constants {
-	public static final List<String> VARIETY_LIST = List.of("I", "J");
+	public static final List<String> VARIETY_LIST = List.of("I", "J", "RB");
 
 }
