40fe018cdf1694bc2cea078174744f3399f3b205
HP <HP@Michael>
2025-01-09 09:42:35 +0800

    update code

--- michael-investment-messagebus/src/main/java/michael/slf4j/investment/message/service/TopicConstants.java
+++ michael-investment-messagebus/src/main/java/michael/slf4j/investment/message/service/TopicConstants.java
@@ -2,5 +2,7 @@ package michael.slf4j.investment.message.service;
 
 public class TopicConstants {
 	public static final String FUTURE_TOPIC = "future-MI-topic";
+	public static final String FUTURE_15M_TOPIC = "future-15M-topic";
+	public static final String FUTURE_30M_TOPIC = "future-30M-topic";
 
 }
